d160ccebb3790e71466dff4496a8653d6c1185fc
Anna Miroshnichenko <anna@alan.app>
2019-09-30 22:59:39 +0500

    Add "Order coffee" intent + minor refactoring

--- voice-scripts/order.alan.voice.js
+++ voice-scripts/order.alan.voice.js
@@ -1,6 +1,7 @@
 visualHints('Say "What can I do here?"',
     'Say "How does this work?"',
     'Say "What does this app do?"',
+    'Say "Order coffee"',
     'Say "How should I use this?"');
 
 intent(`What does this app do?`, `How does this work?`, `What can I do here?`, `How should I use this?`, p => {
@@ -9,25 +10,33 @@ intent(`What does this app do?`, `How does this work?`, `What can I do here?`, `
     p.then(makeOrder);
 });
 
+intent(`Order coffee`, p => {
+    startOrder(p);
+});
+
 let makeOrder = context(() => {
     follow("(probably|)$(ANSWER yes|sure|no)", p => {
         let answer = p.ANSWER.value;
         if (['yes', 'sure'].indexOf(answer) >= 0) {
-            p.play({command: 'start-order'});
-            p.play("We have");
-            p.play({command: 'highlight', data:{item: 'americano'}});
-            p.play("americano");
-            p.play({command: 'highlight', data:{item: 'cappuccino'}});
-            p.play("cappuccino");
-            p.play({command: 'highlight', data:{item: 'latte'}});
-            p.play("and latte");
-            p.play("What drink would you like to order?");
-            p.then(orderCoffee);
+            startOrder(p);
         } else if (answer === 'no') {
             p.play("We sure we can cook a great coffee for you next time.");
         }
     });
 });
+
+function startOrder(p) {
+    p.play({command: 'start-order'});
+    p.play("We have");
+    p.play({command: 'highlight', data:{item: 'americano'}});
+    p.play("americano");
+    p.play({command: 'highlight', data:{item: 'cappuccino'}});
+    p.play("cappuccino");
+    p.play({command: 'highlight', data:{item: 'latte'}});
+    p.play("and latte");
+    p.play("What drink would you like to order?");
+    p.then(orderCoffee);
+}
 
 let orderCoffee = context(() => {
     follow("(I will take|I'd like to order|)$(DRINK americano|cappuccino|latte)",  p => {
